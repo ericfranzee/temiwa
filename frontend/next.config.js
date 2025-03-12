631af65c1c572f5f55d381454df456f1be4812af
Ericfanzee <ericfanzee@gmail.com>
2025-03-12 22:37:32 +0000

    innitial commit

---
+++ frontend/next.config.js
@@ -0,0 +1,42 @@
+/** @type {import("next").NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.uzmart.org",
+        port: "",
+        pathname: "/storage/**",
+      },
+      {
+        protocol: "https",
+        hostname: "foodyman.s3.amazonaws.com",
+        port: "",
+        pathname: "/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "api.demand24.org",
+      },
+    ],
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
